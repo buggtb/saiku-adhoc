b846d683df706b103b3dd9836a04bed2fcacb75a
Tom Barber <tom@analytical-labs.com>
2011-12-15 14:56:24 +0000

    add files

---
+++ saiku-adhoc-webapp/src/main/webapp/js/adhoc/models/Result.js
@@ -0,0 +1,22 @@
+/**
+ * Holds the resultset for a query, and notifies plugins when resultset updated
+ */
+var Result = Backbone.Model.extend({
+    initialize: function(args, options) {
+        // Keep reference to query
+        this.query = options.query;
+    },
+    
+    parse: function(response) {
+
+        this.query.workspace.trigger('query:result', {
+            workspace: this.query.workspace,
+            data: response
+        });
+      
+    },
+    
+    url: function() {
+    	return encodeURI(this.query.url() + "/result");
+    }
+});
